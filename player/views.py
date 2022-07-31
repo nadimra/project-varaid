1497d705e25df5e79480eda59bf9df1a3b9d5784
nadimra <nadimrahman98@gmail.com>
2022-07-31 11:59:23 +0100

    model j

--- player/views.py
+++ player/views.py
@@ -44,7 +44,7 @@ def extractHighlight(request):
         highlightOut = highlightPath+'/'+highlightName +'.mp4'
         highlightOutTemp = highlightPath+'/'+highlightName +'2.mp4'
 
-        modelName = "ModelI"
+        modelName = "ModelJ"
         modelPath = MODEL_ZOO[modelName]
 
         # Save files
